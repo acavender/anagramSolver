d32ab79442ee89cbcb7b71d1ccce73e61f8bbb77
Amy Cavender <acavende@147.53.121.148>
2018-12-04 13:46:24 -0500

    More changes that don't work.

--- anagram.js
+++ anagram.js
@@ -1,10 +1,12 @@
-var enteredText = "";
+// var enteredText = "";
 
 function setText() {
     entered = document.getElementById("anagram").value;
+    return entered;
 }
 
+var enteredText = setText();
+alert(enteredText);
 
-
-var confirm = document.getElementById("confirmation");
-confirm.innerHTML = "You entered " + enteredText + "<br>";
+// var confirm = document.getElementById("confirmation");
+// confirm.innerHTML = "You entered " + enteredText + "<br>";
