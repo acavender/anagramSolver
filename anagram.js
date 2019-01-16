dfa8c7ffd5fcd92b4d6fbd9654ba79c82b29ac28
acavender <acavender@gmail.com>
2019-01-15 20:43:58 -0500

    Note to self about an idea for solving the problem.

--- anagram.js
+++ anagram.js
@@ -1,12 +1,34 @@
-// var enteredText = "";
+// Nothing I've tried so far works, because I haven't yet learned enough.
 
-function setText() {
-    entered = document.getElementById("anagram").value;
-    return entered;
-}
+// A hint to the solution I'm looking for, though, likely lies in this example from a freeCodeCamp lesson (https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/functional-programming/learn-about-functional-programming):
 
-var enteredText = setText();
-alert(enteredText);
+// Note as part of the exercise I changed line 30 from const tea4TeamFCC = null to what appears below.
 
-// var confirm = document.getElementById("confirmation");
-// confirm.innerHTML = "You entered " + enteredText + "<br>";
+// /**
+//  * A long process to prepare tea.
+//  * @return {string} A cup of tea.
+//  **/
+// const prepareTea = () => 'greenTea';
+
+// /**
+//  * Get given number of cups of tea.
+//  * @param {number} numOfCups Number of required cups of tea.
+//  * @return {Array<string>} Given amount of tea cups.
+//  **/
+// const getTea = (numOfCups) => {
+//     const teaCups = [];
+
+//     for (let cups = 1; cups <= numOfCups; cups += 1) {
+//         const teaCup = prepareTea();
+//         teaCups.push(teaCup);
+//     }
+
+//     return teaCups;
+// };
+
+// // Add your code below this line
+// const tea4TeamFCC = getTea(40); // :(
+
+// // Add your code above this line
+
+// console.log(tea4TeamFCC);
