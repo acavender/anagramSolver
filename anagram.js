97de6d83f8d315af9cbc3b441f222288fe044a17
Amy Cavender <acavender@Benden.local>
2020-04-11 11:20:31 -0400

    Try changes that still don't work.

--- anagram.js
+++ anagram.js
@@ -1,34 +1,6 @@
-// Nothing I've tried so far works, because I haven't yet learned enough.
+function setText() {
+    return document.getElementById("anagram").value;
+}
 
-// A hint to the solution I'm looking for, though, likely lies in this example from a freeCodeCamp lesson (https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/functional-programming/learn-about-functional-programming):
-
-// Note as part of the exercise I changed line 30 from const tea4TeamFCC = null to what appears below.
-
-// /**
-//  * A long process to prepare tea.
-//  * @return {string} A cup of tea.
-//  **/
-// const prepareTea = () => 'greenTea';
-
-// /**
-//  * Get given number of cups of tea.
-//  * @param {number} numOfCups Number of required cups of tea.
-//  * @return {Array<string>} Given amount of tea cups.
-//  **/
-// const getTea = (numOfCups) => {
-//     const teaCups = [];
-
-//     for (let cups = 1; cups <= numOfCups; cups += 1) {
-//         const teaCup = prepareTea();
-//         teaCups.push(teaCup);
-//     }
-
-//     return teaCups;
-// };
-
-// // Add your code below this line
-// const tea4TeamFCC = getTea(40); // :(
-
-// // Add your code above this line
-
-// console.log(tea4TeamFCC);
+var jumble = setText();
+console.log(jumble);
