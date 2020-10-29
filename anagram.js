1af91aef82f8f227f9adb4cc14462dfaa82f9bf1
Amy Cavender <acavender@gmail.com>
2020-10-28 20:50:36 -0400

    Succeed in getting the value so it can be worked with

--- anagram.js
+++ anagram.js
@@ -1,8 +1,18 @@
+// This function runs when the user submits the jumble.
 function getEntry() {
-    var entry = document.getElementById("anagram").value;
-    alert(entry);
-    return entry;
+    var jumble = "tfuiot"; /* test case */
+    // var jumble = document.getElementById("anagram").value; /* actual case, when all is ready */
+    console.log(jumble);
+    var arr = jumble.split("");
+    console.log(arr);
+    return arr;
 }
 
-var jumble = getEntry();
-console.log(jumble);
+var working = getEntry();
+console.log(working);
+
+/* The above should now work to capture user input, convert it to an array, and save it to a variable. Next steps:
+(1) Iterate through the possible combinations of letters
+(2) Check those combinations agains an English-language dictionary using an API
+(3) Keep track of any words that can be formed from the letters of the jumble
+(4) Write all the words that can be made to the HTML document */
